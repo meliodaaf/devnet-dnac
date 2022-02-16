e9c1beeb94088b5214bbee57e4342c032620852f
Clarence Subia <subiaclarence96@gmail.com>
2022-02-17 00:46:36 +0800

    Parsed json

--- get_devices.py
+++ get_devices.py
@@ -1,5 +1,5 @@
-import requests
 import json
+import requests
 from get_token import data
 
 url = "https://sandboxdnac.cisco.com/dna/intent/api/v1/network-device/"
@@ -11,4 +11,16 @@ headers = {
 
 response = requests.request("GET", url, headers=headers)
 
-print(response.text)
+data = json.loads(response.text)["response"]
+
+for item in data:
+	hostname = item["hostname"]
+	mac = item["macAddress"]
+	sn = item["serialNumber"]
+	series = item["series"]
+	role = item["role"]
+	type = item["softwareType"]
+
+	output = hostname, mac, sn, series, role, type
+
+	print(output)
