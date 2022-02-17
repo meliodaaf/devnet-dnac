6541946f948d03e14fabb3f32f4874b1de61fe7b
Clarence Subia <subiaclarence96@gmail.com>
2022-02-17 12:46:09 +0800

    Used functions.

--- get_token.py
+++ get_token.py
@@ -1,15 +1,28 @@
+#!/usr/bin/python3
+
 import requests
 import json
 
-url = "https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token"
 
-username = "devnetuser"
-password = "Cisco123!"
+def main():
+	token = get_token()
+	print(token)
 
-headers = {
-  'Content-Type': 'application/json'
-}
+def get_token():
+	
+	# Authentication URL to get the Token that will be used for API calls.
+	url = "https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token"
 
-response = requests.post(url, headers=headers, auth=(username, password))
+	# Basic authentication
+	auth = ("devnetuser", "Cisco123!")
+	headers = {'Content-Type': 'application/json'}
 
-data = json.loads(response.text)["Token"]
+	response = requests.post(url, headers=headers, auth=auth)
+
+	response.raise_for_status()
+	Token = json.loads(response.text)["Token"]
+
+	return Token
+
+if __name__ == '__main__':
+	main()
